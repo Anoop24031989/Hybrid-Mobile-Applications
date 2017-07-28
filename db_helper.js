25138131b884a45dd0a57e8130b7b27c7cb40347
Anoop Varghese <anoopvarghesek@fss.co.in>
2017-07-28 15:18:54 +0530

    my night commit

--- db_helper.js
+++ db_helper.js
@@ -1,11 +1,17 @@
-var client = module.exports = require('mysql').createPool({
+/*var client = module.exports = require('mysql').createPool({
     host : '10.44.78.13',
     port : '3306',
     user : 'testuser1',
     password: 'testuser1',
     database: 'test1'
+});*/
+var client = module.exports = require('mysql').createPool({
+    host : 'sql12.freemysqlhosting.net',
+    port : '3306',
+    user : 'sql12187524',
+    password: 'cSi95BT321',
+    database: 'sql12187524'
 });
-
 
 
 module.exports = client;
