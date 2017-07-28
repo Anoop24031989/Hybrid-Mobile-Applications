b84a042876fcdb4991127c28eb5685439ad6a09d
Anoop Varghese <anoopvarghesek@fss.co.in>
2017-07-28 12:18:44 +0530

    my night commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use('/master', master);
 app.use('/userInfo', userInfo);
 app.use('/cardInfo', cardInfo);
-var server = app.listen(8080, function() {
+var server = app.listen(process.env.PORT || 8080, function() {
 	var host = server.address().address
 	var port = server.address().port
 	console.log("Example app listening at http://%s:%s", host, port)
