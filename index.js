b940c7a5fb6ac7312edb358848dd0fd257b4ad51
Anoop Varghese <anoopvarghesek@fss.co.in>
2017-07-28 11:26:52 +0530

    my tenth commit

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var app = express();
 var master = require('./master');
 var userInfo = require('./UserInfo')
 var cardInfo = require('./cardInfo')
-
 var cors = require('cors')
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json()
@@ -12,42 +11,9 @@ app.use(cors());
 app.use('/master', master);
 app.use('/userInfo', userInfo);
 app.use('/cardInfo', cardInfo);
-
 var server = app.listen(8080, function() {
 	var host = server.address().address
 	var port = server.address().port
 	console.log("Example app listening at http://%s:%s", host, port)
 });
 
-/*var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var path = require('path');
- 
-// Initialize appication with route / (that means root of the application)
-app.get('/', function(req, res){
-  var express=require('express');
-  app.use(express.static(path.join(__dirname)));
-  res.sendFile(path.join(__dirname, '../MADP_NodeJs_API', 'chat.html'));
-});
- 
-// Register events on socket connection
-io.on('connection', function(socket){ 
-//console.log(socket);
-  socket.on('chatMessage', function(from, msg){
-	  console.log('chatMessage');
-	  console.log(from);
-	  console.log(msg);
-    io.emit('chatMessage', from, msg);
-  });
-  socket.on('notifyUser', function(user){
-	  console.log('notifyUser');
-	  console.log(user);
-    io.emit('notifyUser', user);
-  });
-});
- 
-// Listen application request on port 3000
-http.listen(3000, function(){
-  console.log('listening on :3000');
-});*/
